a3038b8dd94f9a798ae936d947d615d8fb16b0a9
firmanmimangsjpl <firman.hidayat@sjpl.co.id>
2025-07-25 11:21:08 +0700

    fix: fix footer default layout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -922,7 +922,7 @@ dir="{{ Helper::determineLanguageDirection() }}">
 
                 <div class="1hidden-xs pull-left">
                     <div class="pull-left">
-                         {!! trans('general.footer_credit') !!}
+                         {{-- {!! trans('general.footer_credit') !!} --}}
                     </div>
                     <div class="pull-right">
                     @if ($snipeSettings->version_footer!='off')
